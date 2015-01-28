2e29cd8725ba14a2deddbb47015ffa852aa05615
Carson <carwbrown@gmail.com>
2015-01-28 13:36:25 -0500

    additions to ex 16

--- ex16.c
+++ ex16.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 // creating a Person structure with four elements
+// Person is the structure's tag and the Members are the variables w/in the structure
 struct Person {
 	char *name;
 	int age;
@@ -11,6 +12,7 @@ struct Person {
 	int weight;
 };
 
+//the creation of the structure by assigning values
 struct Person *Person_create( char *name, int age, int height, int weight)
 {
 	//checking to make sure memory allocated for creation of the person structure
@@ -36,13 +38,24 @@ void Person_destroy(struct Person *who)
 	free(who);
 }
 
+
 //printing the created Person struct to console
 void Person_print (struct Person *who)
 {
+	//to access the members of a structure using a pointer to that structure you must use the -> operator
 	printf("Name: %s\n", who->name);
 	printf("\tAge: %d\n", who->age);
 	printf("\tWeight: %d\n", who->height);
 	printf("\tHeight: %d\n", who->weight);
+}
+
+//print the non pointer structures
+void Person_print1 (struct Person who)
+{
+	printf("Name: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tWeight: %d\n", who.height);
+	printf("\tHeight: %d\n", who.weight);
 }
 
 int main (int argc, char*argv[])
@@ -71,6 +84,21 @@ int main (int argc, char*argv[])
 	//destroy both structures to clean up memory
 	Person_destroy(joe);
 	Person_destroy(frank);
+	
+	//create structures without functions
+	
+	struct Person tom;
+	tom.name = "Tom Thompson";
+	tom.age = 21;
+	tom.weight = 150;
+	tom.height = 55;
+	
+	printf("Person Name: %s\n", tom.name);
+	printf("\tAge: %d\n", tom.age);
+	printf("\tWeight: %d\n", tom.height);
+	printf("\tHeight: %d\n", tom.weight);
+	
+	Person_print1(tom);
 	
 	return 0;
 }
